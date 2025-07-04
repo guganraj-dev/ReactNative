d827c51eabe46c4186e1230b7310091dd7c5b823
guganraj-dev <guganrajofficial@gmail.com>
2025-07-04 15:11:42 +0530

    Initial commit

--- server.js
+++ server.js
@@ -11,11 +11,14 @@ app.use(express.json());
 
 
 
-mongoose.connect(process.env.MONGODB_URI)
-.then(()=>{
-
-console.log('connect')
-})
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('✅ Connected to MongoDB');
+}).catch((err) => {
+  console.error('❌ MongoDB connection error:', err);
+});
 
 
 
